68a95c0684068e51420132a96df3e84c7b9ec325
Tesfa Demissie <tesfadaniel1@gmail.com>
2021-07-19 16:50:39 -0400

    dynamically render overlay items

--- app/js/game.js
+++ app/js/game.js
@@ -21,33 +21,38 @@ export const Start = () => {
 }
 
 const drawOverlay = ({ canvas }) => {
-    // const overlay = canvas.getContext("2d");
-    // overlay.fillStyle = game.border;
-    // // overlay.clearRect(0, 0, canvas.width, canvas.height);
-    // overlay.beginPath();
-    // overlay.fillRect(blockPositionX(1), blockPositionY(1), game.size * 4, game.size * 2);
-    // overlay.closePath();
-    // overlay.fill();
-
     var items = [
-        {},
+        {
+            label: 'Time',
+            value: '34s',
+            id: 'timeKeeper'
+        },
+        {
+            label: 'Score',
+            value: 420,
+            id: 'scoreKeeper'
+        },
+        {
+            label: null,
+            value: 'P/Pause',
+            btn: true,
+            id: 'pauseBtn'
+        }
+
     ]
 
-    var scoreKeeper = `<div class="overlayItem scoreKeeper topLeft">
-            <label>Score</label>
-            <div>420</div>
-        </div>`
+    const className = (item, index) => {
+        var pos = index === 0 ? 'topLeft' :
+            index === 1 ? 'topRight' :
+                index === 2 ? 'bottomRight' : 'bottomLeft'
 
-    var timeKeeper = `<div class="overlayItem timeKeeper bottomLeft">
-        <label>Time</label>
-        <div>34s</div>
-    </div>`
+        var btn = item.btn ? 'btn' : ''
 
-    var exitBtn = `<div class="overlayItem timeKeeper topRight btn"><div>E/Exit</div></div>`
-
-    var pause = `<div class="overlayItem pause bottomRight btn"><div>P/Pause</div></div>`
-
-    document.getElementById('overlay').innerHTML += scoreKeeper;
-    document.getElementById('overlay').innerHTML += exitBtn;
-    document.getElementById('overlay').innerHTML += pause;
+        return `overlayItem ${btn} ${pos}`
+    }
+    items.map((item, index) => {
+        return document.getElementById('overlay').innerHTML += `<div id=${item.id} class="${className(item, index)}">${item.label !== null ? `<label>${item.label}</label>` : ''
+            }
+            <div>${item.value}</div></div > `;
+    })
 }

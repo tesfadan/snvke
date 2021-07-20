bb8757c468ed33866ce4d85a27af81ef691b6c56
Tesfa Demissie <tesfadaniel1@gmail.com>
2021-07-20 03:11:52 -0400

    stop snake food from overlaying on interactive elemnts

--- app/js/placeFood.js
+++ app/js/placeFood.js
@@ -1,6 +1,15 @@
 import { blockPositionX, blockPositionY, random } from "./utils.js";
 import { game, gridNumber, position, session } from "./variables.js";
 
+
+
+const avoidThis = [...session.tails,
+[1, 1], [2, 1], [3, 1],
+[gridNumber.x - 1, 1],
+[gridNumber.x - 2, 1]
+[gridNumber.x - 3, 1]
+]
+
 // Draw Food
 export const placeFood = ({ canvas }) => {
     const food = canvas.getContext('2d');
@@ -13,12 +22,22 @@ export const placeFood = ({ canvas }) => {
     eatFood({ food });
 }
 
+const foodPosition = () => {
+    var pos = { x: random(0, gridNumber.x - 1), y: random(0, gridNumber.y - 1) };
+    var y = avoidThis.filter(item => item[0] === pos.x && item[1] === pos.y);
+    while (y.length > 0) {
+        pos = { x: random(0, gridNumber.x - 1), y: random(0, gridNumber.y - 1) };
+        y = avoidThis.filter(item => item[0] === pos.x);
+    }
+    return pos
+}
+
 
 export const eatFood = ({ food }) => {
     if (position.x === session.foodPosition.x && position.y === session.foodPosition.y) {
         // score();
         food.clearRect(0, 0, game.width, game.height);
-        session.foodPosition = { x: random(0, gridNumber.x - 1), y: random(0, gridNumber.y - 1) };
+        session.foodPosition = foodPosition();
         session.tails.push({ ...session.foodPosition });
     }
 }
